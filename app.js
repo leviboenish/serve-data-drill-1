ad551f0e2b0d6dffca31d9bbc9e373408c564e2e
leviboenish <leviboenish@gmail.com>
2018-07-31 14:29:04 -0600

    port fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 9000;
+const port = process.env.PORT || 9000;
 const cohortData = require('./cohorts')
 
 app.use(cors())
