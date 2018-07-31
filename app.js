71b3e885bc1ff2c66eb4514a4fa05573181fbf10
leviboenish <leviboenish@gmail.com>
2018-07-31 13:51:40 -0600

    error handle added

--- app.js
+++ app.js
@@ -6,25 +6,35 @@ const cohortData = require('./cohorts')
 
 app.use(cors())
 
-app.get('/', (req,res,next) => {
-  res.json({cohortData})
+app.get('/', (req, res, next) => {
+  res.json({
+    cohortData
+  })
 })
 
 const findById = (params, data) => {
-  for(let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     let holderString = (data[i].ID).toString()
-    if(params === holderString){
+    if (params === holderString) {
       return data[i];
     }
   }
   return null
 }
 
-
-
-app.get('/:id', (req,res,next) => {
+app.get('/:id', (req, res, next) => {
   const cohort = findById(req.params.id, cohortData)
-  res.json({cohort})
+  if (!cohort) {
+    res.status = 404;
+    res.json({
+      error: {
+        message: "No ID found!"
+      }
+    })
+  }
+  res.json({
+    cohort
+  })
 })
 
 app.listen(port, () => {
